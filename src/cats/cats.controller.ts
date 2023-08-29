8d60860fba586f4467992cc4aa55b34fa53fd845
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-29 16:21:46 -0300

    adiciona as rotas POST, DELETE e PUT

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Param, ParseIntPipe, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Post, Put, Res } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 import { Response } from 'express';
@@ -18,6 +18,34 @@ export class CatsController {
         const gatoEncontrado = this.catsService.findById(id);
         if (gatoEncontrado) {
             res.status(HttpStatus.OK).json(gatoEncontrado);
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+        const indexGatoEncontrado = this.catsService.findIndexById(id);
+        if (indexGatoEncontrado >= 0) {
+            this.catsService.deleteByIndex(indexGatoEncontrado);
+            res.status(HttpStatus.NO_CONTENT).send();
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
+    }
+
+    @Post()
+    create(@Body() cat: Cat, @Res() res: Response) {
+        this.catsService.create(cat);
+        res.status(HttpStatus.CREATED).json(cat);
+    }    
+
+    @Put(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() cat: Cat, @Res() res: Response) {
+        const indexGatoEncontrado = this.catsService.findIndexById(id);
+        if(indexGatoEncontrado >= 0){
+            this.catsService.update(indexGatoEncontrado, cat);
+            res.status(HttpStatus.NO_CONTENT).send();
         } else {
             res.status(HttpStatus.NOT_FOUND).send();
         }
