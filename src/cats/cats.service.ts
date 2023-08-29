b0f5fa8b6f14e9c1108757c079f0a1c22b3583f8
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-29 15:13:32 -0300

    adiciona controller e service para cats

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { Cat } from './interfaces/cat.interface';
+
+@Injectable()
+export class CatsService {
+
+    private readonly cats: Cat[] = [];
+
+    findAll(): Cat[] {
+        return this.cats;
+    }
+
+}
---
+++ src/cats/interfaces/cat.interface.ts
@@ -0,0 +1,6 @@
+export interface Cat {
+    id: number,
+    nome: string,
+    raca: string,
+    corOlhos: string
+}
