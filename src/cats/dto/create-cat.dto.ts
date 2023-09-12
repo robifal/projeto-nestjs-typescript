420d6f00f3afd3f306c7a5544e48ccd427849438
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-09-12 13:35:35 -0300

    adiciona novas validações em CreateCatDto

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,11 +1,18 @@
-import { IsNotEmpty, MinLength } from "class-validator";
+import { IsAlpha, IsIn, IsNotEmpty, MinLength, NotEquals } from "class-validator";
 
 export class CreateCatDto {
+    
     id: number;
 
     @IsNotEmpty()
-    @MinLength(2)
+    @MinLength(2, {message: 'O nome do gato tem que ter no mínimo 2 caracteres.'})
     nome: string;
+    
+    @IsNotEmpty()
+    @IsIn(['Siamês', 'Sphinx', 'Persa'])
     raca: string;
+
+    @IsNotEmpty()
+    @IsAlpha("pt-BR", {message: 'faiou'})
     corOlhos: string;
 }
