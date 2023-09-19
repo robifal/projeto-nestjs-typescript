37b97a132a6b93136b9876f3e0e2eb58fc561aff
Robert <anavanessa162@gmail.com>
2023-09-19 16:31:19 -0300

    TypeOrm adicionado, ele faz conexão com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-ck4ucomi9prc73agmqd0-a.oregon-postgres.render.com',
+      port: 5432,
+      username: 
+'robert_postgre_exemplo_user',
+      password: '123456',
+      database: 'robert_postgre_exemplo',
+      entities: [],
+      synchronize: true,
+      ssl: true,
+    }), CatsModule],
+
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
